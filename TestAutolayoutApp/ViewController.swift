d9fbfdabef992d232e05ee0fb5395439b1461636
Sawwaty <yezhovye@mac.com>
2021-06-29 23:03:59 +0300

    Initial Commit

---
+++ TestAutolayoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAutolayoutApp
+//
+//  Created by Victoria Klimova on 29.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
