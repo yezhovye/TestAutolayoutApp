56bb2035ecc5b3aca82e78196d8b1db0e5a8aaa8
Sawwaty <yezhovye@mac.com>
2021-07-03 20:05:19 +0300

    test app with ui elements

--- TestAutolayoutApp/ViewController.swift
+++ TestAutolayoutApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelTextLabel: UILabel!
+    @IBOutlet var showTextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelTextLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 10
+        labelTextLabel.textColor = . red
     }
 
 
+    @IBAction func showTextPressedButton(_ sender: UIButton) {
+        if labelTextLabel.isHidden {
+            labelTextLabel.isHidden.toggle()
+            showTextButton.setTitle("Hide Text", for: .normal) }
+        else {
+            labelTextLabel.isHidden.toggle()
+            showTextButton.setTitle("Show Text", for: .normal)
+        }
+        }
 }
 
